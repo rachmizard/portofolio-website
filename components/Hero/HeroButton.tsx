3d9d3b9e935f64cab9718e34da419a3445d30664
Rachmizard <rachmizard11072000@gmail.com>
2021-07-09 22:28:58 +0700

    add onClick type props for button & add handling redirect download cv

--- components/Hero/HeroButton.tsx
+++ components/Hero/HeroButton.tsx
@@ -3,23 +3,31 @@ import { InlineIcon } from '@iconify/react'
 import downloadOutlined from '@iconify/icons-ant-design/download-outlined'
 
 export default function HeroButton() {
-    return (
-        <div className="mt-10 flex space-x-1 lg:space-x-10">
-            <Button type="link" href="#footer">
-                Contact
-            </Button>
-            <Button
-                transparent
-                icon={
-                    <InlineIcon
-                        width={28}
-                        height={28}
-                        icon={downloadOutlined}
-                        className="text-white"
-                    />
-                }>
-                Download CV
-            </Button>
-        </div>
-    )
+     const redirectHandler = () => {
+          window.open(
+               'https://drive.google.com/file/d/11sycBUEJaFBCjmHSjlvtOqS2dTlmYVs_/view?usp=sharing',
+               '_blank'
+          )
+     }
+
+     return (
+          <div className="mt-10 flex space-x-1 lg:space-x-10">
+               <Button type="link" href="#footer">
+                    Contact
+               </Button>
+               <Button
+                    onClick={redirectHandler}
+                    transparent
+                    icon={
+                         <InlineIcon
+                              width={28}
+                              height={28}
+                              icon={downloadOutlined}
+                              className="text-white"
+                         />
+                    }>
+                    Download CV
+               </Button>
+          </div>
+     )
 }
--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -4,54 +4,56 @@ import styles from '../../styles/Button.module.css'
 type TypeEnum = 'button' | 'link'
 
 type Props = {
-    children: React.ReactNode
-    className?: string
-    style?: React.CSSProperties
-    outlined?: boolean
-    transparent?: boolean
-    icon?: React.ReactNode
-    href?: string
-    type?: TypeEnum
+     children: React.ReactNode
+     className?: string
+     style?: React.CSSProperties
+     outlined?: boolean
+     transparent?: boolean
+     icon?: React.ReactNode
+     href?: string
+     type?: TypeEnum
+     onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 export default function Button({
-    children,
-    className,
-    outlined,
-    style,
-    transparent = false,
-    icon = null,
-    href,
-    type = 'button',
+     children,
+     className,
+     outlined,
+     style,
+     transparent = false,
+     icon = null,
+     href,
+     type = 'button',
+     onClick,
 }: Props) {
-    const classes = [className, styles['btn']]
+     const classes = [className, styles['btn']]
 
-    if (outlined) {
-        classes.push(styles['btn-outlined'])
-    } else {
-        classes.push(styles['btn-primary'])
-    }
+     if (outlined) {
+          classes.push(styles['btn-outlined'])
+     } else {
+          classes.push(styles['btn-primary'])
+     }
 
-    if (transparent) {
-        classes.push(styles['btn-transparent'])
-    }
+     if (transparent) {
+          classes.push(styles['btn-transparent'])
+     }
 
-    if (icon !== null) {
-        classes.push(styles['btn-icon'])
-    }
+     if (icon !== null) {
+          classes.push(styles['btn-icon'])
+     }
 
-    if (type === 'link') {
-        return (
-            <a href={href} className={classes.join(' ')}>
-                {children}
-            </a>
-        )
-    }
+     if (type === 'link') {
+          return (
+               <a href={href} className={classes.join(' ')}>
+                    {children}
+               </a>
+          )
+     }
 
-    return (
-        <button style={style} className={classes.join(' ')}>
-            {children}
-            {icon !== null ? <span className="ml-2">{icon}</span> : null}
-        </button>
-    )
+     return (
+          <button onClick={onClick} style={style} className={classes.join(' ')}>
+               {children}
+               {icon !== null ? <span className="ml-2">{icon}</span> : null}
+          </button>
+     )
 }
